0d1002130e60b03a84f4bea1f2ddeb7736460a06
M4D <code@m4dz.net>
2015-04-21 10:05:45 +0200

    update aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,7 +1,5 @@
+alias fuck='$(thefuck $(fc -ln -1))'
 alias r='ranger'
-
 alias fd='mdfind -name'
-
-alias managepy='python manage.py'
-
 alias c='pygmentize -O style=monokai -f console256 -g'
+alias managepy='python manage.py'
